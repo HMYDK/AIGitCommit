577aa76dbad7163aa122fc8624c9a4029df07683
HMYDK <paranoia_zk@yeah.net>
2024-12-31 14:05:30 +0800

    feat(AIGit): 更新 Gemini 模型及版本号
    更新 Gemini 支持的模型版本，并将应用版本号升级到 1.4.1。
    
    - 新增 Gemini 2.0 flash experimental 模型
    - 更新默认 Gemini 模型选择为 "gemini-2.0-flash-exp"
    - 版本号更新至 1.4.1

--- src/main/java/com/hmydk/aigit/config/ApiKeySettings.java
+++ src/main/java/com/hmydk/aigit/config/ApiKeySettings.java
@@ -18,7 +18,7 @@ import java.util.Map;
 @State(name = "com.hmydk.aigit.config.ApiKeySettings", storages = { @Storage("AIGitCommitSettings.xml") })
 public class ApiKeySettings implements PersistentStateComponent<ApiKeySettings> {
     private String selectedClient = "Gemini";
-    private String selectedModule = "gemini-1.5-flash-latest";
+    private String selectedModule = "gemini-2.0-flash-exp";
     private String commitLanguage = "English";
 
     private String promptType = Constants.CUSTOM_PROMPT;
--- src/main/java/com/hmydk/aigit/constant/Constants.java
+++ src/main/java/com/hmydk/aigit/constant/Constants.java
@@ -38,7 +38,7 @@ public class Constants {
 
     public static final Map<String, String[]> CLIENT_MODULES = new HashMap<>() {
         {
-            put(Gemini, new String[]{"gemini-1.5-flash-latest", "gemini-1.5-flash", "gemini-1.5-pro"});
+            put(Gemini, new String[]{"gemini-2.0-flash-exp", "gemini-1.5-flash-latest", "gemini-1.5-flash", "gemini-1.5-pro"});
             put(OpenAI_API, new String[]{""});
             put(Ollama, new String[]{"qwen2.5:14b", "llama3.2:3b"});
             put(CloudflareWorkersAI,
