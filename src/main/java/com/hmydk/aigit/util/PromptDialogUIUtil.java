6b5039ffe65b3c4e0fc984892aebf415f6d057d5
HMYDK <paranoia_zk@yeah.net>
2025-02-14 22:18:46 +0800

    feat: 更新提示信息，支持更多占位符
    添加了对 {diff} 和 {language} 占位符的说明。

--- src/main/java/com/hmydk/aigit/util/PromptDialogUIUtil.java
+++ src/main/java/com/hmydk/aigit/util/PromptDialogUIUtil.java
@@ -1,11 +1,18 @@
 package com.hmydk.aigit.util;
 
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
 import com.intellij.ui.JBColor;
 import com.intellij.ui.components.JBScrollPane;
 import com.intellij.util.ui.JBUI;
-
-import javax.swing.*;
-import java.awt.*;
 
 /**
  * PromptDialogUIUtil
@@ -90,7 +97,9 @@ public class PromptDialogUIUtil {
         gbc.weightx = 1;
         gbc.weighty = 0;
         gbc.fill = GridBagConstraints.HORIZONTAL;
-        JLabel helpTextLabel = new JLabel("Supported placeholders: {diff}, {language}.");
+        JLabel helpTextLabel = new JLabel("<html>Supported placeholders:<br>" +
+                "• {diff} - The code changes in diff format<br>" +
+                "• {language} - The language of the generated commit message</html>");
         helpTextLabel.setForeground(JBColor.GRAY);
         helpTextLabel.setFont(helpTextLabel.getFont().deriveFont(Font.ITALIC));
         panel.add(helpTextLabel, gbc);
