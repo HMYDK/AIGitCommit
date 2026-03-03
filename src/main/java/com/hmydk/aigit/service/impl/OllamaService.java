87b2b3e3531beaaac558442bc3766ed780b1a0d5
Usevalad Buben <usevaladbuben@gmail.com>
2026-03-03 02:33:20 -0800

    Add think field to GenerateRequest class with getter and setter

--- src/main/java/com/hmydk/aigit/service/impl/OllamaService.java
+++ src/main/java/com/hmydk/aigit/service/impl/OllamaService.java
@@ -131,11 +131,13 @@ public class OllamaService implements AIService {
         private String model;
         private String prompt;
         private boolean stream;
+        private boolean think;
 
-        public GenerateRequest(String model, String prompt, boolean stream) {
+        public GenerateRequest(String model, String prompt, boolean stream, boolean think) {
             this.model = model;
             this.prompt = prompt;
             this.stream = stream;
+            this.think = think;
         }
 
         public String getModel() {
@@ -160,6 +162,14 @@ public class OllamaService implements AIService {
 
         public void setStream(boolean stream) {
             this.stream = stream;
+        }
+
+        public boolean isThink() {
+            return think;
+        }
+
+        public void setThink(boolean think) {
+            this.think = think;
         }
     }
 
